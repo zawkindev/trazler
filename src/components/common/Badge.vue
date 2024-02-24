ab57b93e788fd0c3b4f3177cc4ff7dd3e00eab79
zawkindev <zawkindev@gmail.com>
2024-02-24 10:22:29 +0500

    feat: add badge component

---
+++ src/components/common/Badge.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  hasIcon: Boolean
+})
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center w-fit backdrop-blur-sm bg-white/30 rounded-sm text-white"
+    :class="{ 'rounded-full p-2': hasIcon, ' px-2': !hasIcon }"
+  >
+    <slot />
+  </div>
+</template>
--- src/playGround/PlayGround.vue
+++ src/playGround/PlayGround.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from "@/components/layout/Header.vue"
 import Breadcrumb from "@/components/common/Breadcrumb.vue"
+import Badge from "@/components/common/Badge.vue"
 </script>
 
 <template>
@@ -8,5 +9,10 @@ import Breadcrumb from "@/components/common/Breadcrumb.vue"
     <Header> </Header>
 
     <Breadcrumb :data="['salom', 'xayr']" />
+
+    <Badge> salom </Badge>
+    <Badge has-icon>
+      <img class="h-auto w-4" src="@/assets/images/icons/gallery.svg" />
+    </Badge>
   </div>
 </template>
